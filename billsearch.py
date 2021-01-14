c689c681d5a546c3603cad399818569374e0bc6a
vagrant <vagrant@vagrant.vm>
2021-01-14 05:36:22 +0000

    Fixed misreading of what we needed

--- billsearch.py
+++ billsearch.py
@@ -10,22 +10,33 @@ def main(argv):
 
     bills = list()
     for bill_status_file in bill_status_files.namelist():
-        data = bill_status_files.read(bill_status_file)
-        data_str = str(data, 'utf-8', 'ignore')
-        contains = re.search(search_str, data_str)
-        if bool(contains):
-            doc = minidom.parseString(data_str)
-            bill_number = doc.getElementsByTagName("billNumber")[0]
-            bill_type = doc.getElementsByTagName("billType")[0]
-            bill_details = bill_type.firstChild.data + " " + bill_number.firstChild.data
-            bills.append(bill_details)
+        #print(bill_status_file)
+        
+        ## To ignore the __MACOSX files locally
+        if bill_status_file.startswith("__"): 
+            continue
+
+        # Read the summaries and search
+        data = bill_status_files.read(bill_status_file).decode("utf-8")
+        doc = minidom.parseString(data)
+        summaries_elem = doc.getElementsByTagName("summaries")
+        for summary_elem in summaries_elem:
+            text_elem = summary_elem.getElementsByTagName("text")
+            if len(text_elem) > 0:
+                text = text_elem[0].firstChild.data
+                contains = re.search(search_str, text)
+                if bool(contains):
+                    bill_number = doc.getElementsByTagName("billNumber")[0]
+                    bill_type = doc.getElementsByTagName("billType")[0]
+                    bill_details = bill_type.firstChild.data + " " + bill_number.firstChild.data
+                    bills.append(bill_details)
 
     if not bills:
         print("No results!")
     else:
         print("Found the following bills:")
         for bill in bills:
-            print(bill);
+            print(bill)
 
 if __name__ == "__main__":
     main(sys.argv)
